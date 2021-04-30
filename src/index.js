31597e10ee6fd3fb46fa0c3f021f15761ca91b8a
Larysa1387 <kaliuzhna.off@gmail.com>
2021-04-30 17:06:19 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-import './styles.css';
+import cardsTpl from './templates/menu_cards.hbs';
+import menu from '../src/menu.json'
+import '../src/styles.css';
+
+// Add theme switcher
+const switchToggle = document.getElementById('theme-switch-toggle');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+  };
+
+// For checking current theme
+currentTheme();
+
+switchToggle.addEventListener('change', onSwitchClick);
+function onSwitchClick(e) {
+  console.log(e.target.checked);
+  e.target.checked ?
+    document.body.classList.replace(Theme.LIGHT, Theme.DARK)
+    : document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+  localStorage.setItem('theme', document.body.className);
+}
+
+function currentTheme() {
+  const checkTheme = localStorage.getItem('theme');
+  if (checkTheme === Theme.DARK) {
+    switchToggle.checked = true;
+  }
+  if (!checkTheme) {
+    localStorage.setItem('theme', Theme.LIGHT);
+    document.body.classList.add(Theme.LIGHT)
+  } else {
+    document.body.classList.add(checkTheme);
+  }
+}
+
+// Add cards menu
+const menuRef = document.querySelector('.js-menu');
+const markup = createMarkupCards(menu);
+function createMarkupCards(menu) {
+  return cardsTpl(menu);
+  }
+
+menuRef.insertAdjacentHTML('beforeend', markup);
